bd55bf4c30527b56c38565c7a2f0f5b68b2dbccc
Mena Bebawy <eng.mena.lorans@gmail.com>
2022-04-15 07:51:38 +0200

    add api sub-module

--- api/src/main/java/com/mybooks/api/ApiApplication.java
+++ api/src/main/java/com/mybooks/api/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBooksApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyBooksApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
