fc76dedb7cb9a1345d488e225866911e9b27760f
Mena Bebawy <eng.mena.lorans@gmail.com>
2022-04-17 09:34:05 +0200

    add exception handler to BookController and AuthorController

--- api/src/main/java/com/mybooks/api/controller/AuthorNotFoundException.java
+++ api/src/main/java/com/mybooks/api/controller/AuthorNotFoundException.java
@@ -2,6 +2,6 @@ package com.mybooks.api.controller;
 
 public class AuthorNotFoundException extends RuntimeException {
     public AuthorNotFoundException(String id) {
-        super("Could not find author" + id);
+        super("Could not find author " + id);
     }
 }
