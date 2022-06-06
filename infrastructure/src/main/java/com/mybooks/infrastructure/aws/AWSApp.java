786aa07d5f56ea778682ba9b23e9d0e0602918d7
Mena Bebawy <eng.mena.lorans@gmail.com>
2022-06-06 18:35:06 +0200

    remove extra spaces in AWSApp

--- infrastructure/src/main/java/com/mybooks/infrastructure/aws/AWSApp.java
+++ infrastructure/src/main/java/com/mybooks/infrastructure/aws/AWSApp.java
@@ -5,9 +5,7 @@ import software.amazon.awscdk.core.App;
 public class AWSApp {
     public static void main(String[] args) {
         App app = new App();
-
         new DynamodbStack(app, "my-books-dynamodb");
-
         app.synth();
     }
 }
