735e948d8d4aed6922c8c2a32ae5bf89000d0501
mena.bebawy <eng.mena.lorans@gmail.com>
2022-09-14 16:13:07 +0200

    use load balance with eureka

--- resource-service/src/main/java/com/mybooks/resourceservice/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/mybooks/resourceservice/ResourceServiceApplication.java
@@ -3,7 +3,9 @@ package com.mybooks.resourceservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ResourceServiceApplication {
 
