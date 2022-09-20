fd40619aeab7bfe367c5b5bd24da6a525914060d
mena.bebawy <eng.mena.lorans@gmail.com>
2022-09-20 10:33:39 +0200

    isolate aws properties in seperated file and add it to gitignore

--- client-service/src/main/java/com/mybooks/clientservice/config/AwsProperties.java
+++ client-service/src/main/java/com/mybooks/clientservice/config/AwsProperties.java
@@ -2,11 +2,13 @@ package com.mybooks.clientservice.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "aws")
+@ConfigurationPropertiesScan
 public class AwsProperties {
     private final Cognito cognito = new Cognito();
 
